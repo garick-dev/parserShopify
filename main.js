633b0ee99bbb3910055061cd7f90598a859f141c
garick-dev <garickbestvlad@gmail.com>
2021-11-23 01:13:28 +0300

    Update

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const ws = wb.SheetNames;
 const data = xlsx.utils.sheet_to_json(wb.Sheets[ws[0]]);
 
 
-// console.log(data);
+
 
 let result = [];
-
+let test = [];
 for (item of data) {
     let arr = {};
     let objImg = {};
     let arrImg = [];
-
+    let obj = {};
     for (key in item) {
         if (!key.includes("ИЗОБРАЖЕНИЕ"))  {
            arr[key] = item[key];
@@ -22,18 +22,17 @@ for (item of data) {
         }
         else {
             arrImg.push(item[key]);
-            // console.log(arrImg);
-        }
-
-        objImg["ИЗОБРАЖЕНИЕ"] = arrImg;
+        }        
     }
-    result.push(arr, objImg);
+    result = Object.assign(arr, {"ИЗОБРАЖЕНИЕ" : arrImg});
+    test.push(result);
 }
-console.log(result);
+// console.log(test);
+// console.log(obj);
 
 const newWB = xlsx.utils.book_new();
-const newWS = xlsx.utils.json_to_sheet(result);
+const newWS = xlsx.utils.json_to_sheet(test);
 xlsx.utils.book_append_sheet(newWB, newWS, "New Data");
 
 // console.log(newWS);
-xlsx.writeFile(newWB, "newfile.csv");
+xlsx.writeFile(newWB, "newfile.xlsx");
