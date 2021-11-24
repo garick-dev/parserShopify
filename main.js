a3807e6a24cbd817ebf18c2af4336d90581b2aff
garick-dev <pogorelov.frontend@gmail.com>
2021-11-24 18:01:25 +0300

    Add cycle to create product in shopify

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const { Shopify }= require("@shopify/shopify-api");
 const { DataType } = require("@shopify/shopify-api");
-let title = "title";
-let description = "description";
-let tags= ["tag1", "tag2", "tag3"];
-let sku = "264123213214";
-let price = "26";
-let urlImg = "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg";
-let urlArr =
-    {
-        "src": `${urlImg}`,
-    }
-    ;
-
-let optionMaterial = "Серебро"
-let quantity = "3";
-
-const api = async () => {
+// let title = "title";
+// let description = "description";
+// let tags= ["tag1", "tag2", "tag3"];
+// let sku = "264123213214";
+// let price = "26";
+// let urlImg = "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg";
+// let urlArr =[
+//     {
+//         "src": `${urlImg}`,
+//     }, {
+//         "src": `${urlImg}`,
+//     }]
+//     ;
+//
+// let optionMaterial = "Серебро"
+// let quantity = "3";
+//
+const api = async (title, description, tags, urlArr, price, sku) => {
     const client = new Shopify.Clients.Rest("souvenirboutique.myshopify.com", "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");
     const data = await client.post({
         path: 'products',
@@ -24,25 +26,17 @@ const api = async () => {
                         "title":`${title}`,
                         "body_html": `${description}`,
                         "vendor": "Souvenirboutique",
-                        // "product_type":`${test}`,
+                        "product_type":"Apparel & Accessories",
                         "tags":`${tags}`,
-                    //     "images": [
-                    //         {
-                    //         "src":`${urlImg}`,
-                    //         },
-                    //         {
-                    //         "src":`${urlImg}`,
-                    //         }
-                    // ],
-                        "images": [`${urlArr}`],
+                        "images": urlArr,
                         "options": {
                             "name": "Материал",
                         },
                         "variants": [ {
-                                "option1": `${optionMaterial}`,
+                                // "option1": `${optionMaterial}`,
                                 "price": `${price}`,
                                 "sku" : `${sku}`,
-                                "inventory_quantity": `${quantity}`,
+                                // "inventory_quantity": `${quantity}`,
                         }]
                     }
                 },
@@ -51,21 +45,89 @@ const api = async () => {
     // console.log(data.body.product.images);
     console.log(data);
 }
-api();
+// api();
 
 
-// const xlsx = require("xlsx");
+const xlsx = require("xlsx");
 
-// const wb = xlsx.readFile("product.xlsx");
-// const ws = wb.SheetNames;
+const wb = xlsx.readFile("product.xlsx");
+const ws = wb.SheetNames;
+
+const data = xlsx.utils.sheet_to_json(wb.Sheets[ws[0]]);
+
+let result = [];
+let arrObj = {};
+
+for (let item of data) {
+    let title = "";
+    let description = "";
+    let tags= [];
+    let sku = "";
+    let price = "";
+    let urlArr =[];
+
+    // let optionMaterial = "Серебро"
+    let quantity = "3";
+
+    for (let key in item) {
+        if (key.includes("НАЗВАНИЕ")) {
+            title = item[key];
+        } else if (key.includes("ОПИСАНИЕ")) {
+            description = item[key];
+        }
+        else if (key.includes("ТЕГИ")) {
+            tags.push(item[key]);
+        }
+        else if (key.includes("SKU")) {
+            sku = item[key];
+        }
+        else if (key.includes("ЦЕНА")) {
+            price = item[key];
+        }
+        else if (key.includes("КОЛИЧЕСТВО")) {
+            let num = 0;
+            num = item[key];
+            quantity = num.toString();
+        }
+        else if (key.includes("ИЗОБРАЖЕНИЕ")) {
+            let obj = {};
+            obj = Object.assign(obj,{"src" : item[key]});
+            urlArr.push(obj);
+        }
+
+    }
+  // setTimeout( () =>  api(title, description, tags, price, sku, urlArr), 3000 ) ;
+    console.log("TITLE",title);
+    console.log("DESC", description);
+    console.log("TAGS", tags);
+    console.log("price", price);
+    console.log("SKU", sku);
+    console.log("quantity", typeof quantity);
+    console.log("IMG", urlArr);
+
+}
+
+// console.log(data);
+
+
+
+
+
+// const newWB = xlsx.utils.book_new();
+// const newWS = xlsx.utils.json_to_sheet(result);
+// xlsx.utils.book_append_sheet(newWB, newWS, "New Data");
 //
-// const data = xlsx.utils.sheet_to_json(wb.Sheets[ws[0]]);
-//
+// // console.log(newWS);
+// xlsx.writeFile(newWB, "file.xlsx");
+
+
+
+//////OLD CODE//////////
+
 // let result = [];
 // let arrObj = {};
 // for (item of data) {
 //     let arr = {};
-//     let i = 0;
 //     for (key in item) {
 //         if (!key.includes("ИЗОБРАЖЕНИЕ")) {
 //             arr[key] = item[key];
@@ -77,12 +139,5 @@ api();
 //         }
 //     }
 // }
-// console.log(result);
-
-
-// const newWB = xlsx.utils.book_new();
-// const newWS = xlsx.utils.json_to_sheet(result);
-// xlsx.utils.book_append_sheet(newWB, newWS, "New Data");
-//
-// // console.log(newWS);
-// xlsx.writeFile(newWB, "file.xlsx");
+// console.log(data);
+//////OLD CODE//////////
